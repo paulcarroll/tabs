5efb3d04a158f6f28c833d102fa06db3ca8e5c11
Paul Carroll <paul.carroll@gmail.com>
2023-07-21 18:42:39 +1000

    feat: add column override selector

--- js/song-view.js
+++ js/song-view.js
@@ -2,7 +2,21 @@
 /**
  * 
  */
-function initSongView() {
+function initSongView({ columns }) {
+
+  $(`.song__columns a[data-columns="${columns}"]`).addClass('--active');
+  $(`.song__columns a`).on('click', function(e) {
+    const columns = $(this).data('columns');
+
+    $(`.song__columns a`).removeClass('--active');
+
+    $(this).addClass('--active');
+
+    $('.song-body').removeClass('--force-columns-1 --force-columns-2 --force-columns-3');
+    $('.song-body').addClass(`--force-columns-${columns}`);
+  });
+
+  // Set the playlist controls
   const playlist = getPlaylist();
 
   if (!playlist.length) {
@@ -12,13 +26,16 @@ function initSongView() {
 
   const index = playlist.findIndex(song => window.location.pathname.toLowerCase() === song.url.toLowerCase());
 
+  // Hide playlist if current song is not in playlist
   if (index === -1) {
     $('.sound__playlist').hide();
     return;
   }
 
+  // Set current song of index in playlist
   $('.sound__playlist span.count').text(`${index + 1} of ${playlist.length}`);
 
+  // Disable prev/next button if current song is first/last song in playlist
   if (index === 0) {
     $('.sound__playlist_prev').addClass('--disabled');
   }
